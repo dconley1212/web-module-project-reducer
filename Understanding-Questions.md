903484c69ce2800abfc644cfac2cd3c49945ae77
David Conley <dconley1989@gmail.com>
2022-01-17 14:55:39 -0700

    finished all of the reducer and action cases and event handlers for each action

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,13 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- the event handler starts executing the dispatch function.
+- Within the dispatch function it executes addOne function that returns an action object
+- then the reducer function starts to be executed
+- Execute the action of the reducer function
+- re-render application
+  ...
+
+- TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,90 @@
-import React from 'react';
+import React, { useReducer } from "react";
+import reducer, { initialState } from "../reducers";
+import "./App.css";
 
-import './App.css';
-
-import TotalDisplay from './TotalDisplay';
-import CalcButton from './CalcButton';
+import TotalDisplay from "./TotalDisplay";
+import CalcButton from "./CalcButton";
+import { addOne, applyNumber, changeOperation, clearDisplay } from "../actions";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleNumberChange = (number) => {
+    dispatch(applyNumber(number));
+  };
+
+  const handleOperatorChange = (operator) => {
+    dispatch(changeOperation(operator));
+  };
+
+  const handleClearChange = () => {
+    dispatch(clearDisplay());
+  };
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"> Reducer Challenge</a>
+        <a className="navbar-brand" href="#">
+          {" "}
+          Reducer Challenge
+        </a>
       </nav>
 
-      <div className = "container row mt-5">
+      <div className="container row mt-5">
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
-            
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
-            </div>
-            
-            <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <span id="operation">
+                <b>Operation:</b> {state.operation}
+              </span>
+              <span id="memory">
+                <b>Memory:</b> {state.memory}
+              </span>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={"M+"} />
+              <CalcButton value={"MR"} />
+              <CalcButton value={"MC"} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={1} onClick={() => handleNumberChange(1)} />
+              <CalcButton value={2} onClick={() => handleNumberChange(2)} />
+              <CalcButton value={3} onClick={() => handleNumberChange(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={4} onClick={() => handleNumberChange(4)} />
+              <CalcButton value={5} onClick={() => handleNumberChange(5)} />
+              <CalcButton value={6} onClick={() => handleNumberChange(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={7} onClick={() => handleNumberChange(7)} />
+              <CalcButton value={8} onClick={() => handleNumberChange(8)} />
+              <CalcButton value={9} onClick={() => handleNumberChange(9)} />
+            </div>
+
+            <div className="row">
+              <CalcButton
+                value={"+"}
+                onClick={() => handleOperatorChange("+")}
+              />
+              <CalcButton
+                value={"*"}
+                onClick={() => handleOperatorChange("*")}
+              />
+              <CalcButton
+                value={"-"}
+                onClick={() => handleOperatorChange("-")}
+              />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => handleClearChange()} />
             </div>
-
           </form>
         </div>
       </div>
